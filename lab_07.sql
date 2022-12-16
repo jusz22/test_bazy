f0f477177e1240e64f8e86e836d00ac12111c21b
jusz22 <115210558+jusz22@users.noreply.github.com>
2022-12-16 10:27:32 +0100

    Add files via upload

--- lab_07.sql
+++ lab_07.sql
@@ -1,3 +1,10 @@
 # Zadanie 1
-# a)
+# b)
 select idKreatury from kreatura k left join uczestnicy u on k.idKreatury = u.id_uczestnika where u.id_uczestnika is null;
+# c)
+select distinct w.nazwa, sum(e.ilosc) from wyprawa w join uczestnicy u on w.id_wyprawy = u.id_wyprawy join ekwipunek e on e.idKreatury = u.id_uczestnika group by w.nazwa;
+# Zadanie 2
+# a)
+select count(u.id_uczestnika), group_concat(k.nazwa) from wyprawa w join uczestnicy u on w.id_wyprawy = u.id_wyprawy join kreatura k on k.idKreatury = u.id_uczestnika group by w.id_wyprawy;
+# b) 
+select w.nazwa, s.nazwa, ew.kolejnosc, k.nazwa as kierownik from wyprawa w join kreatura k on w.kierownik = idKreatury join etapy_wyprawy ew on ew.idWyprawy = w.id_wyprawy join sektor s on s.id_sektora = ew.sektor order by w.data_rozpoczecia asc, ew.kolejnosc desc;
